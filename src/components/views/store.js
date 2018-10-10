6803f8c41d480163305df5fee378f51d56021f25
Tibblist <Tibblist@gmail.com>
2018-10-10 13:29:55 -0500

    commit2

--- src/components/views/store.js
+++ src/components/views/store.js
@@ -19,11 +19,9 @@ class Items extends React.Component {
             {createItemArray().map(function(item, id) {
                 console.log(id);
                 console.log(item);
-                var column = "item" + counter;
+                var columnNum = counter % 3;
+                var column = "item" + columnNum;
                 counter++;
-                if (counter == 3) {
-                    counter = 0;
-                }
                 return <li className={column} key={id}>{item[1]}</li>
             })}
             </div>
@@ -34,11 +32,7 @@ class Items extends React.Component {
 function createItemArray() {
     var itemArray = [];
     for (var i = 0; i < 10; i++) {
-        var item = [];
-        item.push(i);
-        item.push("Naglfar");
-        item.push("1.2B");
-        item.push("IMAGE URL HERE");
+        var item = [i, "naglfar", "1.2B", "https://wiki.eveuniversity.org/images/thumb/f/f9/Naglfar.jpg/256px-Naglfar.jpg"];
         itemArray.push(item);
     }
     return itemArray;
