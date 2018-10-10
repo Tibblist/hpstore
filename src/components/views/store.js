09e66ccbbeeab18952106a4bdbf9c07ff1951fe9
Tibblist <Tibblist@gmail.com>
2018-10-10 16:30:46 -0500

    sidebar functionally works

--- src/components/views/store.js
+++ src/components/views/store.js
@@ -36,14 +36,185 @@ class Items extends React.Component {
 }
 
 class SideMenu extends React.Component {
+    constructor(){
+        super()
+        this.state = {
+          capitals: [
+            {
+                id: 0,
+                title: 'All',
+                selected: false,
+                key: 'capitals'
+            },
+            {
+              id: 1,
+              title: 'Amarr',
+              selected: false,
+              key: 'capitals'
+            },
+            {
+              id: 2,
+              title: 'Caldari',
+              selected: false,
+              key: 'capitals'
+            },
+            {
+              id: 3,
+              title: 'Gallente',
+              selected: false,
+              key: 'capitals'
+            },
+            {
+              id: 4,
+              title: 'Minmatar',
+              selected: false,
+              key: 'capitals'
+            },
+            {
+              id: 5,
+              title: 'Faction',
+              selected: false,
+              key: 'capitals'
+            }
+          ],
+          supercapitals: [
+            {
+                id: 0,
+                title: 'All',
+                selected: false,
+                key: 'supercapitals'
+            },
+            {
+              id: 1,
+              title: 'Amarr',
+              selected: false,
+              key: 'supercapitals'
+            },
+            {
+              id: 2,
+              title: 'Caldari',
+              selected: false,
+              key: 'supercapitals'
+            },
+            {
+              id: 3,
+              title: 'Gallente',
+              selected: false,
+              key: 'supercapitals'
+            },
+            {
+              id: 4,
+              title: 'Minmatar',
+              selected: false,
+              key: 'supercapitals'
+            },
+            {
+              id: 5,
+              title: 'Faction',
+              selected: false,
+              key: 'supercapitals'
+            }
+          ],
+          fullfits: [
+            {
+                id: 0,
+                title: 'All',
+                selected: false,
+                key: 'fullfits'
+            },
+            {
+              id: 1,
+              title: 'Amarr',
+              selected: false,
+              key: 'fullfits'
+            },
+            {
+              id: 2,
+              title: 'Caldari',
+              selected: false,
+              key: 'fullfits'
+            },
+            {
+              id: 3,
+              title: 'Gallente',
+              selected: false,
+              key: 'fullfits'
+            },
+            {
+              id: 4,
+              title: 'Minmatar',
+              selected: false,
+              key: 'fullfits'
+            },
+            {
+              id: 5,
+              title: 'Faction',
+              selected: false,
+              key: 'fullfits'
+            }
+          ],
+        }
+    }
+
+    toggleSelected(id, key){
+        let temp = this.state[key]
+        temp[id].selected = !temp[id].selected
+        this.setState({
+          [key]: temp
+        })
+    }
+    
     render() {
         return (
             <div className="sidenav">
                 <ul>
-                    <div>Capitals</div>
-                    <div>Supercapitals</div>
-                    <div>Full Fits</div>
+                    <DropDownButton
+                        title="Capitals"
+                        list={this.state.capitals}
+                    />
+                    <DropDownButton
+                        title="Supercapitals"
+                        list={this.state.supercapitals}
+                    />
                 </ul>
+            </div>
+        );
+    }
+}
+
+class DropDownButton extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+          listOpen: false,
+          headerTitle: this.props.title
+        }
+    }
+    handleClickOutside(){
+        this.setState({
+          listOpen: false
+        })
+    }
+      
+    toggleList(){
+        this.setState(prevState => ({
+          listOpen: !prevState.listOpen
+        }))
+    }
+
+    render() {
+        const{list} = this.props;
+        const{listOpen, headerTitle} = this.state;
+        return (
+            <div className="dd-wrapper">
+                <div className="dd-header" onClick={this.toggleList.bind(this)}>
+                    <div className="dd-header-title">{headerTitle}</div>
+                </div>
+                {listOpen && <ul className="dd-list">
+                    {list.map((item) => (
+                        <li className="dd-list-item" key={item.id} >{item.title}</li>
+                    ))}
+                </ul>}
             </div>
         );
     }
