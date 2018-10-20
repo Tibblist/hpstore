faf260d197d0eeb6709e2b47dad8937eef0dc984
Tibblist <Tibblist@gmail.com>
2018-10-20 00:29:29 -0500

    Update src/components/views/home.js

--- src/components/views/home.js
+++ src/components/views/home.js
@@ -1,40 +1,13 @@
 import React from 'react';
-import { DB } from '../../backend/db';
 
 
 export default class Home extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-          count: DB.getCount()
-        }
-    }
-
-    shouldComponentUpdate(nextProps, nextState) {
-        return this.state.count != nextState.count;
-    }
-
-    testCount() {
-        DB.testCount();
-    }
 
     render() {
         return (
             <div>
                 Hello World!
-                <button onClick={this.testCount}>
-                    Click me!
-                </button>
-                <CountView count={DB.getCount()}></CountView>
             </div>
-        );
-    }
-}
-
-class CountView extends React.Component {
-    render() {
-        return (
-            <div>props:{this.props.count}</div>
         );
     }
 }
