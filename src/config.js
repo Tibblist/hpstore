269b93f5977542c579038a406f933bf78a8695ee
Tibblist <Tibblist@gmail.com>
2018-11-18 18:33:41 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 export const SERVER_URL = "https://login.eveonline.com/oauth/authorize/?response_type=code&redirect_uri=http%3A%2F%2Flocalhost:80%2Fcallback";
 export const CLIENT_ID = "c568354f648e47f2a5f1d54b1c0d297f";
+var test = 0;
